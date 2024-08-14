85537a64d349f792adf87a2fde72b48ec028a0a5
myncepu <myncepu@gmail.com>
2024-08-14 18:11:25 +0800

    modify page style, remove padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ImageInverter from '@/components/ImageInverter'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <ImageInverter />
     </main>
   )
